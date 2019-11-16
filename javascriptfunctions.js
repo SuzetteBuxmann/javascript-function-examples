6a00c8f1d89d47c23b0694afcad3349862670f07
Suzette Buxmann <suzetteb@gmail.com>
2019-11-16 14:49:26 +0000

    Add files via upload

--- javascriptfunctions.js
+++ javascriptfunctions.js
@@ -126,6 +126,21 @@ function reverseString(str) {
 	return string;
 }
 
+function isPalindrome(str) {
+	var origString, splitStr, reverseStr, joinStr, checkPal;
+	origString = str.toLowerCase();
+	splitStr = str.split("");
+	reverseStr = splitStr.reverse();
+	joinStr = reverseStr.join("");
+	checkPal = joinStr.toLowerCase();
+	console.log(origString);
+	console.log(checkPal);
+	return (checkPal === origString);
+}
+
+isPalindrome('hello');
+isPalindrome('kayak');
+
 //Comparator function to sort a javascript array of objects.
 var arrObjs = [
 	{radius: 5, name: 'Pete'}, 
@@ -166,3 +181,101 @@ function lengthOfArray(arr) {
 }
 lengthOfArray(testArray);
 // output: 6
+
+/******************************************/
+//Constructor functions and prototype
+/******************************************/
+
+function Circle(r) {
+	this.radius = r;
+	//this.area = function() {return Math.PI * this.radius * this.radius}; - added with prototype below instead
+}
+var c1 = new Circle(2);
+
+Circle.prototype.area = function() {
+	return Math.PI * this.radius * this.radius;
+}
+var c2 = new Circle(3);
+c1.area();
+c2.area();
+
+/******************************************/
+
+function Shape() {
+	this.area = function() {
+		return 'You need to implement this!';
+	}
+}
+function Square(length) {
+	this.length = length;
+	this.area = function() {
+		return this.length * this.length;
+	}
+}
+function Rectangle(height, width) {
+	this.height = height;
+	this.width = width;
+}
+Square.prototype = new Shape();
+new Square(2).area();
+//output: 4
+Rectangle.prototype = new Shape();
+new Rectangle(4, 6).area();
+//output: "You need to implement this!"
+
+function Rectangle(height, width) {
+	this.height = height;
+	this.width = width;
+	this.area = function() {
+		return this.height * this.width;
+	}
+}
+new Rectangle(4, 6).area();
+//output: 24
+
+/******************************************/
+
+//Write a constructor function to represent a Rectangle, which can calculate its area and perimeter.
+//area = width x length, perimeter = 2(length x width)
+
+function Rectangle(l, w) {
+	this.length = l;
+	this.width = w;
+	this.area = function() {
+		return this.length * this.width;
+	}
+	this.perimeter = function() {
+		return 2 * (this.length * this.width);
+	}
+}
+var rect1 = new Rectangle(2, 2);
+rect1.area(); //4
+rect1.perimeter(); //8
+
+/******************************************/
+
+//Javascript Constructor function evaluates the distance from another point. 
+
+function Point(lat1, lon1) {
+	this.lat1 = lat1;
+	this.lon1 = lon1;
+	this.goSomewhere = function(lat2, lon2) {
+		var R = 6371; //km - use 3,958.8 for miles
+		var dLat = deg2rad(lat2-lat1);
+		var dLon = deg2rad(lon2-lon1);
+		var a = 
+			 Math.sin(dLat/2) * Math.sin(dLat/2) +
+			 Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+			 Math.sin(dLon/2) * Math.sin(dLon/2);
+		var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+		var d = R * c;
+		return d;
+	}
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
+
+var letsGo = new Point(51.5048, -0.1235);
+letsGo.goSomewhere(51.5033, -0.0720);
